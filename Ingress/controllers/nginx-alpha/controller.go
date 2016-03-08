929b42091fd2f78a674e078776ea2f6f9d188b48
Deependra Ariyadewa <deependra@gmail.com>
2016-03-08 14:59:11 +0000

    https support initail commit

--- Ingress/controllers/nginx-alpha/controller.go
+++ Ingress/controllers/nginx-alpha/controller.go
@@ -27,6 +27,7 @@ import (
 	"k8s.io/kubernetes/pkg/apis/extensions"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
 	"k8s.io/kubernetes/pkg/util"
+	"fmt"
 )
 
 const (
@@ -40,18 +41,44 @@ http {
   server_names_hash_max_size 512;
   server_names_hash_bucket_size 64;
 
+{{range $ing := .Items}}
+{{range $rule := $ing.Spec.Rules}}
   server {
     listen 80;
-    server_name services.cloud.wso2.com;
+    server_name {{$rule.Host}};
+{{ range $path := $rule.HTTP.Paths }}
+    location {{$path.Path}} {
+      proxy_set_header Host $host;
+      proxy_pass http://{{$path.Backend.ServiceName}}.{{$ing.Namespace}}.svc.cluster.local:{{$path.Backend.ServicePort}};
+    }{{end}}
+  }{{end}}{{end}}
+}`
+
+	nginxSSLConf = `
+events {
+  worker_connections 1024;
+}
+http {
+  # http://nginx.org/en/docs/http/ngx_http_core_module.html
+  types_hash_max_size 2048;
+  server_names_hash_max_size 512;
+  server_names_hash_bucket_size 64;
 
 {{range $ing := .Items}}
 {{range $rule := $ing.Spec.Rules}}
+  server {
+    listen 443;
+    server_name {{$rule.Host}};
+
+    ssl_certificate      /etc/nginx/cert.pem;
+    ssl_certificate_key  /etc/nginx/cert.key;
+
 {{ range $path := $rule.HTTP.Paths }}
-    location {{$path.Path}}/ {
-      proxy_pass http://{{$path.Backend.ServiceName}}.{{$ing.Namespace}}.svc.cluster.local/;
+    location {{$path.Path}} {
+      proxy_set_header Host $host;
+      proxy_pass http://{{$path.Backend.ServiceName}}.{{$ing.Namespace}}.svc.cluster.local:{{$path.Backend.ServicePort}};
     }{{end}}
-{{end}}{{end}}
-  }
+  }{{end}}{{end}}
 }`
 )
 
@@ -70,14 +97,31 @@ func main() {
 		ingClient = kubeClient.Extensions().Ingress(api.NamespaceAll)
 	}
 	tmpl, _ := template.New("nginx").Parse(nginxConf)
+	tmplSSL, _ := template.New("nginx").Parse(nginxSSLConf)
 	rateLimiter := util.NewTokenBucketRateLimiter(0.1, 1)
 	known := &extensions.IngressList{}
 
 	// Controller loop
-	shellOut("nginx")
+	//shellOut("nginx")
 	for {
 		rateLimiter.Accept()
 		ingresses, err := ingClient.List(api.ListOptions{})
+
+			fmt.Printf("Port %+v\n", ingresses.Items[0].Spec.Rules[0].HTTP.Paths[0].Backend.ServicePort.IntValue())
+			fmt.Printf("Path %+v\n", ingresses.Items[0].Spec.Rules[0].HTTP.Paths[0].Path)
+
+		var httpIngressList extensions.IngressList
+		var httpsIngressList extensions.IngressList
+
+		for _, v := range ingresses.Items {
+			fmt.Printf("%#v\n", v)
+			if v.Spec.Rules[0].HTTP.Paths[0].Backend.ServicePort.IntVal == 80 {
+				httpIngressList.Items = append(httpIngressList.Items,v)
+			} else if v.Spec.Rules[0].HTTP.Paths[0].Backend.ServicePort.IntVal == 443 {
+				httpsIngressList.Items = append(httpsIngressList.Items,v)
+			}
+		}
+
 		if err != nil {
 			log.Printf("Error retrieving ingresses: %v", err)
 			continue
@@ -86,11 +130,18 @@ func main() {
 			continue
 		}
 		known = ingresses
-		if w, err := os.Create("/etc/nginx/nginx.conf"); err != nil {
+		if w, err := os.Create("/etc/nginx/conf.d/appcloud.conf"); err != nil {
 			log.Fatalf("Failed to open %v: %v", nginxConf, err)
-		} else if err := tmpl.Execute(w, ingresses); err != nil {
+		} else if err := tmpl.Execute(w, httpIngressList); err != nil {
 			log.Fatalf("Failed to write template %v", err)
 		}
-		shellOut("nginx -s reload")
+
+		if w, err := os.Create("/etc/nginx/conf.d/appcloud-ssl.conf"); err != nil {
+			log.Fatalf("Failed to open %v: %v", nginxSSLConf, err)
+		} else if err := tmplSSL.Execute(w, httpsIngressList); err != nil {
+			log.Fatalf("Failed to write template %v", err)
+		}
+
+	//	shellOut("nginx -s reload")
 	}
 }
